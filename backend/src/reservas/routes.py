3c77d4c8683a02cc765e8e5167695ff89ce7b307
mlosas <mlosas@fi.uba.ar>
2026-05-20 18:38:34 -0300

    Se agrega endpoints de reserva

--- backend/src/reservas/routes.py
+++ backend/src/reservas/routes.py
@@ -26,8 +26,36 @@ def get_disponibilidad():
       return error("500","Internal Server Error", f"Ocurrio un error al obtener la disponibilidad: {err}", 500)
 
 
+@reservas_bp.route("/reservas", methods=["POST"])
+def create_reserva():
+    reserva_data = request.json
 
-         
+    if not reserva_data:
+        return error("400","Bad Request","No se enviaron datos",400)
+
+    campos_obligatorios = [ 
+        "cliente_nombre",
+        "cliente_email",
+        "cantidad_personas",
+        "fecha",
+        "hora_inicio"
+      ]
+    for campo in campos_obligatorios:
+        if campo not in reserva_data:
+            return error("400","Bad Request",f"El campo {field} es obligatorio", 400)
+
+    try:
+        reserva = service.create_reserva(reserva_data)
+
+        return jsonify({
+            "message": "Reserva creada correctamente","data": reserva}), 201)
+    except ValueError as err:
+        return error("404", "Not Found", str(err), 404)
+    except mysql.connector.Error as err:
+        return error("500", "Internal Server Error", f"No se pudo conectar con la base de datos: {err}", 500)
+    except Exception as err:
+        return error("500", "Internal Server Error", f"Ocurrió un error al crear la reserva: {err}", 500)
+   
                                          
 
                         
