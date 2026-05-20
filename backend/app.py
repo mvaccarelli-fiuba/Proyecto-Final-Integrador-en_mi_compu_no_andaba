a2769a7c83cd931ebf3d0f55c2607324f4e9d0d9
mvaccarelli-fiuba <mvaccarelli@fi.uba.ar>
2026-05-20 15:29:33 -0300

    agrego los enpoints de resenias

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from src.auth.routes import auth_bp
 from src.platos.routes import platos_bp
-
+from src.resenas.routes import resenas_bp
+    
 # A medida que cada dominio se mergee a main, descomentar la linea.
 # from src.mesas.routes import mesas_bp
 # from src.reservas.routes import reservas_bp
-# from src.resenas.routes import resenas_bp
+
 # from src.servicios.routes import servicios_bp
 # from src.dashboard.routes import dashboard_bp
 
@@ -13,10 +14,11 @@ app = Flask(__name__)
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(platos_bp)
+app.register_blueprint(resenas_bp)
 
 # app.register_blueprint(mesas_bp)
 # app.register_blueprint(reservas_bp)
-# app.register_blueprint(resenas_bp)
+
 # app.register_blueprint(servicios_bp)
 # app.register_blueprint(dashboard_bp)
 
