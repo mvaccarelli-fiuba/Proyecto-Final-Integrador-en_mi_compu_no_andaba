4d5c72071c9e9777ea5d306ad19a2b4672f7879e
mlosas <mlosas@fi.uba.ar>
2026-05-20 18:20:49 -0300

    Se agrega endpoints de reserva

--- backend/src/reservas/service.py
+++ backend/src/reservas/service.py
@@ -1,4 +1,17 @@
+import random
 from src.reservas import repository
+
 
 def get_disponibilidad(cantidad_personas):
     return repository.get_disponibilidad(cantidad_personas)
+
+
+def create_reserva(reserva_data):
+    token = str(random.randint(100000, 999999))
+    reserva_data["token"] = token
+    nueva_reserva = repository.create_reserva(reserva_data)
+
+    return {
+        "id": nueva_reserva,
+        "token": token
+    }
