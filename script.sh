d69cc52a68a19499db77461f0fe4cceafa7e7550
ssuas <terodriguez@fi.uba.ar>
2026-05-20 16:20:23 -0300

    Script en desarrollo

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 sudo systemctl start mariadb
-sudo mariadb < Backend/db/setup.sql
-sudo mariadb krusty_krab < Backend/db/schema.sql 
+sudo mariadb < backend/db/user.sql
+sudo mariadb krusty_krab < backend/db/schema.sql 
