5f7290ba54c0561c095bb6f3ea7aecc9f678339b
mlosas <mlosas@fi.uba.ar>
2026-06-01 16:30:30 -0300

    Se agrega endpoints de reserva

--- backend/src/reservas/routes.py
+++ backend/src/reservas/routes.py
@@ -16,21 +16,21 @@ def get_disponibilidad():
        return error("400","Bad request","El parametro cantidad_personas es obligatorio",400)
 
                    
-   try:
-       mesas = service.get_disponibilidad(int(cantidad_personas))
-       return jsonify(mesas), 200
+    try:
+        mesas = service.get_disponibilidad(int(cantidad_personas))
+        return jsonify(mesas), 200
 
-   except mysql.connector.Error as err:
-       return error("500","Internal Server Error", f"No se pudo conectar con la base de datos: {err}", 500)
+    except mysql.connector.Error as err:
+        return error("500","Internal Server Error", f"No se pudo conectar con la base de datos: {err}", 500)
 
    
-   except Exception as err:
-      return error("500","Internal Server Error", f"Ocurrio un error al obtener la disponibilidad: {err}", 500)
+    except Exception as err:
+       return error("500","Internal Server Error", f"Ocurrio un error al obtener la disponibilidad: {err}", 500)
 
 
 @reservas_bp.route("/reservas", methods=["POST"])
 def create_reserva():
-    reserva_data = request.json
+    reserva_data = request.form.to.dict()
 
     if not reserva_data:
         return error("400","Bad Request","No se enviaron datos",400)
@@ -89,7 +89,7 @@ def cancelar_reserva(token):
 @reservas_bp.route("/admin/reservas/consumir", methods=["POST"])
 @require_admin
 def consumir_reserva():
-    data = request.json
+    data = request.form.to.dict()
 
     if not data or "token" not in data:
         return error("400", "Bad request", "El token es obligatorio",400)
